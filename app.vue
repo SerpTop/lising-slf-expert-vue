7bfb2bfe65643aca19f3ffcd2ce4dc5febf9e551
a1skama <a.kadyrova@serptop.ru>
2025-09-08 23:45:08 +0300

    add project

---
+++ app.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex flex-col bg-gray-400">
+    <div class="overflow-hidden content">
+      <HeaderComponent />
+
+
+      <CalculateBlock />
+
+      <FooterComponent />
+
+      <CookiesComponent />
+
+
+
+
+    </div>
+  </div>
+</template>
